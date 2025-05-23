3ea963d6b1a4ce12361f1ff4ea0b9249b8187c84
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-05-23 14:28:28 +0300

    fix: solve IndexError - input text is too long

--- app/summarizer.py
+++ app/summarizer.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_text(text):
-    summary = summarizer(text, max_length=120, min_length=80, do_sample=False)
+    summary = summarizer(text, max_length=120, min_length=80, do_sample=False, truncation=True)
     return summary[0]['summary_text']
 
 if __name__ == "__main__":
